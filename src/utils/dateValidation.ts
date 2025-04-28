998f6df32764a857adbbf225d1eb0bf410383933
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-04-28 09:38:42 -0300

    feat: create functions to validate if the date is valid
    It were created two functions: one to verify if the year
    is a leap year, and the other to verify if the day is
    according to month and year.

---
+++ src/utils/dateValidation.ts
@@ -0,0 +1,37 @@
+export function isDayAccordingToMonthAndYear(
+  day: number,
+  month: number,
+  year: number
+): boolean {
+  const isAThirdOneDaysMonth =
+    (day <= 31 && month === 1) ||
+    (day <= 31 && month === 3) ||
+    (day <= 31 && month === 5) ||
+    (day <= 31 && month === 7) ||
+    (day <= 31 && month === 8) ||
+    (day <= 31 && month === 10) ||
+    (day <= 31 && month === 12);
+
+  const isThirdDaysMonth =
+    (day <= 30 && month === 4) ||
+    (day <= 30 && month === 6) ||
+    (day <= 30 && month === 9) ||
+    (day <= 30 && month === 11);
+
+  const isTwentyEightDaysMonth = day <= 28 && month === 2 && !isLeapYear(year);
+
+  const isTwentyNineDaysMonth = day <= 29 && month === 2 && isLeapYear(year);
+
+  if (
+    !isThirdDaysMonth &&
+    !isAThirdOneDaysMonth &&
+    !isTwentyEightDaysMonth &&
+    !isTwentyNineDaysMonth
+  )
+    return false;
+  else return true;
+}
+
+export function isLeapYear(year: number): boolean {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
