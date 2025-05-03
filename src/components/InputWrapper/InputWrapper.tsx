617b9e3f8d0b0d06d65bcdbbeb4a493398f20202
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-03 11:15:06 -0300

    refact: correct inputWrapper width style on desktop and mobile viewport

--- src/components/InputWrapper/InputWrapper.tsx
+++ src/components/InputWrapper/InputWrapper.tsx
@@ -23,7 +23,7 @@ const InputWrapper: React.FC<InputWrapperBase> = ({
         value={value}
         className={`${
           isErrorShow ? "border-red-400" : "border-gray-300"
-        } border rounded p-4 max-w-40 my-2 focus:outline-purple-500 text-2xl sm:w-[100%]`}
+        } border rounded p-4 w-full min-w-20 my-2 focus:outline-purple-500 text-2xl md:max-w-40`}
         onChange={handleChange}
       />
       <small
